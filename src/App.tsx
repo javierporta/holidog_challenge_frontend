c1ebd9b4af6d5e8867676edfae755c94d7286ac4
Javier <2190271@my.ipleiria.pt>
2020-07-29 14:39:30 +0200

    Home page styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Home } from './home/home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header text-center">
         <img src={logo} className="App-logo mt-2" alt="logo" />
         <h1>Holidog Challange - Frontend</h1>
+        <small>Made with react create by Javier Portaluppi</small>
       </header>
+      <Home />
     </div>
   );
 }
