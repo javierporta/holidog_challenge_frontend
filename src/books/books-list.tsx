65ba8d79b64335345cc5b72d933170ce4ab405c0
Javier <2190271@my.ipleiria.pt>
2020-07-29 16:49:27 +0200

    Change listof books by proper table with buttons

--- src/books/books-list.tsx
+++ src/books/books-list.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent, useEffect } from 'react'; // importing FunctionComponent
 import { Book } from './book';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/esm/Button';
 
 interface BookProps {
     books: Array<Book>;
@@ -14,13 +16,28 @@ export const BooksList: FunctionComponent<BookProps> = ({ books }: { books: Arra
 
     return <aside>
         {books && books.length > 0 &&
-            <ul>
-                {books.map(book => (
-                    <li key={book._id}>
-                        <span>{book.name}</span>
-                    </li>
-                ))}
-            </ul>
+            <div className="row justify-content-center">
+                <div className="col-auto">
+                    <Table striped bordered hover variant="dark" className="table table-responsive">
+                        <thead>
+                            <tr>
+                                <th>Book Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {books.map(book => (
+                                <tr key={book._id}>
+                                    <td>{book.name}</td>
+                                    <td><Button>View</Button></td>
+                                    <td><Button>Edit</Button></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </div>
+            </div>
+
+
         }
     </aside>
 }
